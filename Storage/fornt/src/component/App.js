ffe1ac9e3591c17e61576fc81804f962306ce7a2
swo1548 <swo1548@gmail.com>
2020-03-18 17:36:04 +0900

    (front) 예제 작성

--- Storage/fornt/src/component/App.js
+++ Storage/fornt/src/component/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // style
 import './App.css'
 
+// firebase
+import firebase from 'firebase';
+const firebaseConfig = {
+  apiKey: "AIzaSyB48vgNoCW2K26PlG_BWS4xuu2kWdBBt1U",
+  authDomain: "kick-storage-exam.firebaseapp.com",
+  databaseURL: "https://kick-storage-exam.firebaseio.com",
+  projectId: "kick-storage-exam",
+  storageBucket: "kick-storage-exam.appspot.com",
+  messagingSenderId: "596019196668",
+  appId: "1:596019196668:web:b7cb12fcfa32c2ffcc1d79",
+  measurementId: "G-R7XWD1WWZR"
+};
+firebase.initializeApp(firebaseConfig);
+
 const App = () => {
+  const [fileURL, setFileURL] = useState("");
+
+  const fireStorage = firebase.storage();
+  const StorageRef = fireStorage.ref();
+  const fileRef = StorageRef.child("brian.jpg");
+
+  fileRef.getDownloadURL().then(url => setFileURL(url));
+
   return (
     <div>
       hello world!
+      <img src={fileURL} /> 
     </div>
   )
 }
